e90770661b824069e3eb3f905cddd73a257fe28d
yofed2 <yofed2@gmail.com>
2017-02-25 11:03:18 -0500

    module 9 work in progress, add travis stuff

---
+++ src/index.test.js
@@ -0,0 +1,22 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('our first test', () => {
+  it ('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+//  test is asyn, so need to pass in "done", and tell mocha at the end it is done, then will do the "expect"
+describe('index.html', () => {
+  it('should say hello', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    jsdom.env(index, function(err, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect (h1.innerHTML).to.equal("Hello World!");
+      done();
+      window.close();
+    });
+  })
+})
